1c523ef62edf5a21925913ba8dc783b6fe2960c6
Ratiu <ratiualexcristian@gmail.com>
2021-06-24 21:20:46 +0300

    cleanup not used code

--- functions.js
+++ functions.js
@@ -12,22 +12,23 @@ console.info(getInfo("Alex"));
  console.info(wellcome);
 
  function showHome() {
-    document.getElementById('home').style.display = 'block';
+    
     document.getElementById('skills').style.display='none';
     document.getElementById('projects').style.display = 'none';
     document.getElementById('languages').style.display='none';
+    document.getElementById('home').style.display = 'block';
 }
  function showSkills() {
     document.getElementById('home').style.display = 'none';
-    document.getElementById('skills').style.display='block';
     document.getElementById('projects').style.display = 'none';
     document.getElementById('languages').style.display='none';
+    document.getElementById('skills').style.display='block';
 }
 function showProjects() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('skills').style.display = 'none';
-    document.getElementById('projects').style.display='block';
     document.getElementById('languages').style.display='none';
+    document.getElementById('projects').style.display='block';
 }
 function showLanguages() {
     document.getElementById('home').style.display = 'none';
