83f0d7e8bb0d6533cd132b9a0426cd339a0c583c
Ratiu <ratiualexcristian@gmail.com>
2021-06-24 21:43:15 +0300

    add hide page function

--- functions.js
+++ functions.js
@@ -1,39 +1,28 @@
-console.debug("Inside functions.js")
-
-function getInfo(name) {
-    var mess = "Salut "+name;
-    console.warn("Inside get Info", mess);
-    return mess;
-} 
-console.info(getInfo("Alex"));
- var myName="ALEX";
- console.info(getInfo(myName));
- var wellcome = getInfo(myName);
- console.info(wellcome);
-
+ function hidePage(id) {
+    console.info('hide page', id);
+    document.getElementById(id).style.display='none';
+ }
  function showHome() {
-    
-    document.getElementById('skills').style.display='none';
-    document.getElementById('projects').style.display = 'none';
-    document.getElementById('languages').style.display='none';
+    hidePage("skills");
+    hidePage("projects");
+    hidePage("languages");
     document.getElementById('home').style.display = 'block';
 }
  function showSkills() {
-    document.getElementById('home').style.display = 'none';
-    document.getElementById('projects').style.display = 'none';
-    document.getElementById('languages').style.display='none';
-    document.getElementById('skills').style.display='block';
+    hidePage("home");
+    hidePage("projects");
+    hidePage("languages");
+    document.getElementById('skills').style.display = 'block';
 }
 function showProjects() {
-    document.getElementById('home').style.display = 'none';
-    document.getElementById('skills').style.display = 'none';
-    document.getElementById('languages').style.display='none';
-    document.getElementById('projects').style.display='block';
+    hidePage("home");
+    hidePage("skills");
+    hidePage("languages");
+    document.getElementById('projects').style.display = 'block';
 }
 function showLanguages() {
-    document.getElementById('home').style.display = 'none';
-    document.getElementById('skills').style.display = 'none';
-    document.getElementById('projects').style.display = 'none';
-    document.getElementById('languages').style.display='block';
-
+    hidePage("home");
+    hidePage("skills");
+    hidePage("projects");
+    document.getElementById('languages').style.display = 'block';
 }
