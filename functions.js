c286b55ace58ba2be2bc2267935951d89303d0e1
Ratiu <ratiualexcristian@gmail.com>
2021-07-05 09:15:16 +0300

    Highlight active menu

--- functions.js
+++ functions.js
@@ -4,6 +4,20 @@
  function show(id) {
     document.getElementById(id).style.display='block';
  }
+ function highlight(el) {
+  //var oldBorderColor = el.style.borderColor;
+  //console.warn('oldBorderColor %o', oldBorderColor)
+  el.style.borderColor = '#00df00';
+  setTimeout(function () {
+    el.style.borderColor = '#55df44';
+  }, 100);
+  setTimeout(function () {
+    el.style.borderColor = '#99df88';
+  }, 200);
+  setTimeout(function () {
+    el.style.borderColor = '';
+  }, 300);
+}
  function hideAllPages() {
     var pages = Array.from(document.getElementsByClassName('page'));
     pages.forEach(function(page){
