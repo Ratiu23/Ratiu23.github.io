2dd9c52d17b22b0383bf7b53b2024900df863152
Ratiu <ratiualexcristian@gmail.com>
2021-07-05 21:10:54 +0300

    Print favorite skills

--- functions.js
+++ functions.js
@@ -54,11 +54,18 @@ document.querySelector('#top-menu-bar').addEventListener("click", function(e){
 })
 
 
-var skills= ["html", "js", "css"];
+var skills= [
+  {name:"html", favorit: true}, 
+  {name: "js" },
+  {name: "css", favorit: true}
+];
 
 var skillsHtml= skills.map(function(skill){
   console.info(skill);
-  return `<li>${skill}</li>`;
+  if (skill.favorit) {
+    return `<li class="favorit">${skill.name}</li>`;
+  }
+  return(`<li>${skill.name}</li>`);
 }).join('');
 
 document.querySelector("#skills ul").innerHTML= skillsHtml;
